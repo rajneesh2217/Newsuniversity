e40939f9d3c8329df1f86b003d6da4ba395250b1
Rajneesh kumar <rajnish221712@gmail.com>
2022-02-17 10:41:41 +0530

    Updates

---
+++ precache-manifest.9625ce6683fd47e00f57330254375b6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a63c21989af73b5a5b6563e22ef217b",
+    "url": "/Newsuniversity.github.io/index.html"
+  },
+  {
+    "revision": "4206a02422cbe2aa2c92",
+    "url": "/Newsuniversity.github.io/static/css/main.3c076489.chunk.css"
+  },
+  {
+    "revision": "d42d46cc71e2bbc35ae1",
+    "url": "/Newsuniversity.github.io/static/js/2.305d98b5.chunk.js"
+  },
+  {
+    "revision": "5601ff8807d1512972f0",
+    "url": "/Newsuniversity.github.io/static/js/3.cd3a1805.chunk.js"
+  },
+  {
+    "revision": "4206a02422cbe2aa2c92",
+    "url": "/Newsuniversity.github.io/static/js/main.a400edbd.chunk.js"
+  },
+  {
+    "revision": "dcade949ff9d424c2b35",
+    "url": "/Newsuniversity.github.io/static/js/runtime~main.def278dd.js"
+  },
+  {
+    "revision": "444c0d9cf29d7ea460bff38539512cab",
+    "url": "/Newsuniversity.github.io/static/media/loading.444c0d9c.gif"
+  }
+]);
